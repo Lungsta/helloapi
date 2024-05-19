82b4491c3d10b6b51f814b1e948646998000db4a
Ntethelelo Lungelo Ndaba <lungelo.ndaba@sanlam.co.za>
2024-05-19 22:21:33 +0200

    Added create employees post.

--- src/main/java/za/co/zalumabo/helloapi/controller/EmployeeController.java
+++ src/main/java/za/co/zalumabo/helloapi/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package za.co.zalumabo.helloapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import za.co.zalumabo.helloapi.model.Employee;
@@ -19,5 +21,10 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 }
