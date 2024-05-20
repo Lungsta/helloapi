fa9e44c3867eeaeab4b4783f7fbc00bc99297b44
Ntethelelo Lungelo Ndaba <lungelo.ndaba@sanlam.co.za>
2024-05-20 20:56:09 +0200

    Added update and findByID controller methods.

--- src/main/java/za/co/zalumabo/helloapi/controller/EmployeeController.java
+++ src/main/java/za/co/zalumabo/helloapi/controller/EmployeeController.java
@@ -1,14 +1,19 @@
 package za.co.zalumabo.helloapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import za.co.zalumabo.helloapi.exception.ResourceNotFoundException;
 import za.co.zalumabo.helloapi.model.Employee;
 import za.co.zalumabo.helloapi.repository.EmployeeRepository;
+import za.co.zalumabo.helloapi.service.EmployeeService;
 
 import java.util.List;
 
@@ -18,6 +23,9 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Autowired
+    EmployeeService employeeService;
 
     @CrossOrigin
     @GetMapping("/employees")
@@ -29,5 +37,23 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    @CrossOrigin
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee" +
+                " does not exist with id : " + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    @CrossOrigin
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee" +
+                " does not exist with id : " + id));
+        Employee updateedEmployee = employeeService.updateEmployee(employee, employeeDetails);
+
+        return ResponseEntity.ok(updateedEmployee);
     }
 }
---
+++ src/main/java/za/co/zalumabo/helloapi/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package za.co.zalumabo.helloapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.co.zalumabo.helloapi.model.Employee;
+import za.co.zalumabo.helloapi.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public Employee updateEmployee (Employee employee, Employee employeeDetails) {
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+
+        return employeeRepository.save(employee);
+    }
+}
